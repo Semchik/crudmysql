7a6a8fcf0449455c52b51a5138e459d8954c925d
Vitaliy Semchyshyn <sem4yshyn.new@gmail.com>
2018-05-29 19:36:46 +0300

    Init commit

---
+++ src/test/java/com/den/crudsql/CrudsqlApplicationTests.java
@@ -0,0 +1,16 @@
+package com.den.crudsql;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CrudsqlApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
